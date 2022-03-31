bf5316882f007bf1238cc729e7e8534238c27f19
lanlyhs <ziqiang.chen@outlook.com>
2022-03-31 19:23:59 +0800

    Update add java variable

---
+++ src/main/java/com/github/syntax/Variables.java
@@ -0,0 +1,15 @@
+package com.github.syntax;
+
+public class Variables {
+
+    int instanceVariables;
+    static String classVariables;
+    static final int CONSTANT_VARIABLE = 6;
+
+    void instanceMethod(int parameterVariables) {
+    }
+
+    static void staticMethod() {
+    }
+
+}
