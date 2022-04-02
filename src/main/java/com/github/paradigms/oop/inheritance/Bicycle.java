86f8e2769b289dd2aa3228b25565cc111ce21837
lanlyhs <ziqiang.chen@outlook.com>
2022-04-02 09:47:49 +0800

    Update change field private

--- src/main/java/com/github/paradigms/oop/inheritance/Bicycle.java
+++ src/main/java/com/github/paradigms/oop/inheritance/Bicycle.java
@@ -2,28 +2,34 @@ package com.github.paradigms.oop.inheritance;
 
 public class Bicycle {
 
-    // the Bicycle class has
-    // three fields
-    public int cadence;
-    public int gear;
-    public int speed;
+    private int cadence;
+    private int gear;
+    private int speed;
 
-    // the Bicycle class has
-    // one constructor
     public Bicycle(int startCadence, int startSpeed, int startGear) {
         gear = startGear;
         cadence = startCadence;
         speed = startSpeed;
     }
 
-    // the Bicycle class has
-    // four methods
+    public int getCadence() {
+        return cadence;
+    }
+
     public void setCadence(int newValue) {
         cadence = newValue;
     }
 
+    public int getGear() {
+        return gear;
+    }
+
     public void setGear(int newValue) {
         gear = newValue;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 
     public void applyBrake(int decrement) {
--- src/main/java/com/github/paradigms/oop/inheritance/MountainBike.java
+++ src/main/java/com/github/paradigms/oop/inheritance/MountainBike.java
@@ -2,19 +2,13 @@ package com.github.paradigms.oop.inheritance;
 
 class MountainBike extends Bicycle {
 
-    // the MountainBike subclass has
-    // one field
     public int seatHeight;
 
-    // the MountainBike subclass has
-    // one constructor
     public MountainBike(int startHeight, int startCadence, int startSpeed, int startGear) {
         super(startCadence, startSpeed, startGear);
         seatHeight = startHeight;
     }
 
-    // the MountainBike subclass has
-    // one method
     public void setHeight(int newValue) {
         seatHeight = newValue;
     }
