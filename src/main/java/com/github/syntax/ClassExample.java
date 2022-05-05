433ff6437ca5014d3412bc028f076f16223b1a7a
Lanlyhs <ziqiang.chen@outlook.com>
2022-05-05 09:08:26 +0800

    Update add static block

--- src/main/java/com/github/syntax/ClassExample.java
+++ src/main/java/com/github/syntax/ClassExample.java
@@ -2,6 +2,53 @@ package com.github.syntax;
 
 public class ClassExample {
 
+    // class initialization
+    // 1
+    static {
+    }
+
+    // 2
+    public static int myVar = initializeClassVariable();
+    private static int initializeClassVariable() {
+        // initialization code goes here
+        return 0;
+    }
+
+    // instance initial
+    // 1. constructor
+    // 2. blocks
+    // The Java compiler copies initializer blocks into every constructor. Therefore, this approach can be used to share a block of code between multiple constructors.
+    {
+    }
+    // 3.
+    private int myVar1 = initializeInstanceVariable();
+    protected final int initializeInstanceVariable() {
+        // initialization code goes here
+        return 0;
+    }
+
+    private int cadence;
+    private int gear;
+    private int speed;
+
+    private final int id;
+
+    private static int numberOfBicycles = 0;
+
+    public ClassExample(int startCadence,
+                   int startSpeed,
+                   int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+
+        id = ++numberOfBicycles;
+    }
+
+    public int getID() {
+        return id;
+    }
+
     // Overloading Methods
     // method signatures = method name + method parameter list
     // Note: Overloaded methods should be used sparingly, as they can make code much less readable.
