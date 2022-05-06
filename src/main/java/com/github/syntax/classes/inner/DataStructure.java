84069c39572d125a3f1f00aa7ad3d906f4909f1e
Lanlyhs <ziqiang.chen@outlook.com>
2022-05-06 22:07:35 +0800

    Update local class

---
+++ src/main/java/com/github/syntax/classes/inner/DataStructure.java
@@ -0,0 +1,47 @@
+package com.github.syntax.classes.inner;
+
+import java.util.Iterator;
+
+public class DataStructure {
+
+    private final static int SIZE = 15;
+    private final int[] arrayOfInts = new int[SIZE];
+
+    public DataStructure() {
+        for (int i = 0; i < SIZE; i++) {
+            arrayOfInts[i] = i;
+        }
+    }
+
+    public void printEven() {
+        EvenIterator iterator = this.new EvenIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + "");
+        }
+        System.out.println();
+    }
+
+    interface DataStructureIterator extends Iterator<Integer> {
+    }
+
+    private class EvenIterator implements DataStructureIterator {
+
+        private int nextIndex = 0;
+
+        public boolean hasNext() {
+            return (nextIndex <= SIZE - 1);
+        }
+
+        public Integer next() {
+            Integer retValue = arrayOfInts[nextIndex];
+            nextIndex += 2;
+            return retValue;
+        }
+    }
+
+    public static void main(String[] args) {
+        DataStructure ds = new DataStructure();
+        ds.printEven();
+    }
+
+}
